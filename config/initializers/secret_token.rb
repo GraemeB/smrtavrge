df8ad36bb9a245af2b080db48c469ff9239d1817
Graeme <graemebarnes1987@gmail.com>
2013-07-16 12:48:21 +0200

    SmartAverage MVP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smrtavrge::Application.config.secret_token = 'f53efd87c95dba2fe73e8944fff053b53827e8c04ad09d236764116c2f2f582a8fc0aebc5a97ad78b6a1257d82b98086570c98fac8b6af13170e0cdf4af252f0'
